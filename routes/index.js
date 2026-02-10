e9e1e98ff6704bf8d67ef76035d85c6689553db1
Spencer <sweinstein34@gmail.com>
2026-02-10 12:23:03 -0500

    Added product router to routes/index.js + commented out shop/category features for now

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./auth.js";
 import usersRouter from "./users.js";
+import productsRouter from "./products.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/users", usersRouter);
+router.use("/products", productsRouter);
 
 export default router;
