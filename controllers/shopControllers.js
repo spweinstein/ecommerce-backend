6cec98ce4abd2b4303339bc9a97878ddf4f0dd00
Spencer <sweinstein34@gmail.com>
2026-02-12 15:25:14 -0500

    Made changes to getProducts so can filter by category

--- controllers/shopControllers.js
+++ controllers/shopControllers.js
@@ -2,7 +2,9 @@ import Shop from "../models/shop.js";
 
 const getShops = async (req, res) => {
   try {
-    const shops = await Shop.find({}).populate("industry");
+    const query = {};
+    if (req.query.industry) query.industry = req.query.industry;
+    const shops = await Shop.find(query).populate("industry");
     return res.status(200).json(shops);
   } catch (error) {
     return res.status(500).json({ error: error.message });
